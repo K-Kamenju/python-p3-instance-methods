ac58dfdcdaf0a80d993362c80125c9f988b1dec7
Caleb <kamenjuhkiiru@gmail.com>
2023-12-04 05:59:00 +0300

    Completed assignment and revision

--- lib/dog.py
+++ lib/dog.py
@@ -6,15 +6,15 @@ class Dog:
     #Instance method definition
     def bark(self):
         print("Woof!")
-
+    
     def sit(self):
         print("The dog is sitting.")
-
-
+        
+    
+        
 fido = Dog()
 fido.bark()
 fido.sit()
-
 snoopy = Dog()
 snoopy.bark()
 snoopy.sit()
--- lib/person.py
+++ lib/person.py
@@ -6,11 +6,6 @@ class Person:
     #Instance method definition
     def talk(self):
         print("Hello World!")
-    
+        
     def walk(self):
         print("The person is walking.")
-
-
-kamenju = Person()
-kamenju.talk()
-kamenju.walk()
